6eb152a75b6ad731e80595a0e845168447a1935e
Manuel <manuelejdelatorre@gmail.com>
2021-03-05 15:39:52 -0300

    Value is changing on cells

--- src/components/Spreadsheet.js
+++ src/components/Spreadsheet.js
@@ -4,60 +4,70 @@ import Table from 'react-bootstrap/Table'
 const Spreadsheet = () => {
     const [header, setHeader] = useState(["#", "A", "B", "C", "D", "E", "F"]);
     const [column, setColumn] = useState([1, 2, 3, 4, 5])
-    const [value, setValue] = useState([{
-        1: {
-            A1: "",
-            B1: "",
-            C1: "",
-            D1: "",
-            E1: "",
-            F1: ""
-        },
+    const [value, setValue] = useState(
+        [{
+            1: {
+                A1: "1",
+                B1: "",
+                C1: "",
+                D1: "",
+                E1: "",
+                F1: ""
+            },
 
-        2: {
-            A2: "",
-            B2: "",
-            C2: "",
-            D2: "",
-            E2: "",
-            F2: ""
+            2: {
+                A2: "",
+                B2: "",
+                C2: "",
+                D2: "",
+                E2: "",
+                F2: ""
 
-        },
+            },
 
-        3: {
-            A3: "",
-            B3: "",
-            C3: "",
-            D3: "",
-            E3: "",
-            F3: ""
-        },
+            3: {
+                A3: "",
+                B3: "",
+                C3: "",
+                D3: "",
+                E3: "",
+                F3: ""
+            },
 
-        4: {
-            A4: "",
-            B4: "",
-            C4: "",
-            D4: "",
-            E4: "",
-            F4: ""
-        },
+            4: {
+                A4: "",
+                B4: 3,
+                C4: "",
+                D4: "",
+                E4: "",
+                F4: ""
+            },
 
-        5: {
-            A5: "",
-            B5: "",
-            C5: "",
-            D5: "",
-            E5: "",
-            F5: ""
-        }
+            5: {
+                A5: "",
+                B5: "",
+                C5: "",
+                D5: "",
+                E5: "",
+                F5: ""
+            }
 
 
 
-    }])
+        }])
 
-    const handleChange = (e) => {
-
+    const handleChange = (e, number, key) => {
+        setValue([...value], value[0][number][key] = e.target.value)
+        console.log(value)
     }
+
+
+
+
+    // console.log(newData)
+    // setValue(newData)
+
+
 
     const sum = (e) => {
 
@@ -67,6 +77,7 @@ const Spreadsheet = () => {
 
 
     return (
+        // <div>{handleChange()}</div>
         <Table bordered >
             <thead >
                 <tr>
@@ -77,41 +88,26 @@ const Spreadsheet = () => {
 
                 {column.map((number) => (
                     <tr>
+
                         <td>{number}</td>
-                        {value.map((cell) => (
-                            Object.values(cell[number]).map((value) => (
-                                <td>{value}</td>
+                        {value.map((cell, idx) => (
+                            Object.entries(cell[number]).map((val) => (
+
+                                < td >
+                                    {console.log(val)}
+                                    <input className="input"
+                                        value={val[1]}
+                                        name={number}
+                                        onChange={(e) => handleChange(e, number, val[0])}
+                                    />
+
+                                </td>
+
                             )
                             )))}
                     </tr>
                 )
                 )}
-                {/* <td>1</td>
-                    <td ><input className="input" value= {value[1].A1} /> </td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td> */}
-
-                {/* <tr>
-                    <td>2</td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td ><input className="input" /></td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td>
-                    <td><input className="input" /></td>
-                </tr> */}
             </tbody>
         </Table >
     )
